c3d2a8ccb1f19668b425ae6e90ae89796b754689
Evan Floden <evanfloden@gmail.com>
2021-05-31 22:01:52 +0200

    Removed work dir

--- nf-training/work/07/7e0ef21507ddb73f7bc4d9672d646b/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-echo t_coffee /Users/cwyatt/Desktop/Nextflow_practise/nf-training-public/nf-training/data/prots/BB11001.tfa > Mouse.aln
--- nf-training/work/9a/243bb0f838f627b3c5ad3741641b87/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-echo t_coffee /Users/cwyatt/Desktop/Nextflow_practise/nf-training-public/nf-training/data/prots/BB11003.tfa > Mouse.aln
